362fd5b11c783f5d797d9b78510028349c6278ec
jeremyp72330 <jeremy.pascaud@bts-malraux.net>
2015-01-21 12:04:34 +0100

    Mon premier truc

---
+++ main.c
@@ -0,0 +1,12 @@
+double operandeA , operandeB, resultat;
+
+cout << " Entrer votre premier operateur " << endl;
+cin >> operandeA;
+
+cout << " Entrer votre deuxieme operateur " << endl;
+cin >> operande B;
+
+
+
+resultat = operandeA * operandeB;
+
